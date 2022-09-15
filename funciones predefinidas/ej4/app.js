2f140f0af51d8da0c283741849750bb950536715
Elena2332 <elenafesional@gmail.com>
2022-09-15 12:35:40 +0200

    parte del 4

--- funciones predefinidas/ej4/app.js
+++ funciones predefinidas/ej4/app.js
@@ -1,21 +1,29 @@
-function cambio(color)
-{
-    document.body.style.backgroundColor=color;
-}
-
-/*******  Forma con listener 
-
-let chBlue;
-
+let p1, p2, p3, p4, texto;
 window.onload
 {
-    chBlue=document.getElementById("blue");
-    chBlue.addEventListener("click", cambio);
+    p1=document.getElementById("p1");
+    p2=document.getElementById("p2");
+    p3=document.getElementById("p3");
+    p4=document.getElementById("p4");
 }
 
-function cambio()
-{
-    document.body.style.backgroundColor=this.id;
-}
-*/
+//parrafo 1
+texto = "La resolucion de la pantalla es: "+screen.width+"x"+screen.height;
+p1.innerHTML= texto;
 
+
+//parrafo2
+texto = "El nombre del navegador que usas es: "+navigator.appName ;
+texto = "<br> Usas el siguiente sistema operativo: " ;
+texto = "<br> La version del navegador que usas es: "+navigator.appVersion  ;
+p2.innerHTML= texto;
+// ayudita de omar:   https://didesweb.com/javascript/datos-navegador-javascript/
+
+
+//parrafo3
+texto = "La URL del documento es: " ;
+p3.innerHTML= texto;
+
+//parrafo4
+texto= "El protocolo usado para acceder a esta pagina es: " ;
+p4.innerHTML= texto;
