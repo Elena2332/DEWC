7c4a5ec149f5d7328a55a9aa65786ef60dc26550
Elena2332 <elenafesional@gmail.com>
2022-09-28 19:55:37 +0200

    ejercicio5 funciona
    calculadora terminada

--- DOM/ej5/app.js
+++ DOM/ej5/app.js
@@ -1,10 +1,13 @@
 //eval("str");     calcular funcion
-let buttons; 
+let buttons;
+let operadores = ["+","-","*","/"]; 
+let numeros = ["1","2","3","4","5","6","7","8","9","0"];
 let inpTxt;
+let btnAnterior = "";
 
 window.onload = function() 
 {
-    inpTxt.getElementById("inpNums");
+    inpTxt = document.getElementById("inpNums");
     buttons = document.getElementsByTagName("button");
     for(let i=0; i<buttons.length; i++)
         buttons[i].addEventListener("click", pulsar);
@@ -12,6 +15,35 @@ window.onload = function()
 
 function pulsar()
 {
-    let txtBtn= this.textContent;
-    
+    let txtBtn= this.value;
+    if(operadores.includes(txtBtn))  // si es un operador
+    {
+        if(numeros.includes(btnAnterior) || btnAnterior == '=')   // si ya hay un numero 
+            inpTxt.value = inpTxt.value+txtBtn;     
+    }
+    else
+    {
+        if(numeros.includes(txtBtn))   // si es un numero
+            inpTxt.value = inpTxt.value+txtBtn;
+        else
+        {
+            if(txtBtn == 'C')   // si es C 
+                inpTxt.value = "";   // vaciar input
+            else  
+            {   // si es '='
+                if(operadores.includes(btnAnterior))  // si lo ultimo es un operador
+                    inpTxt.value = inpTxt.value.substring(0, inpTxt.value.length-1);
+                else
+                {
+                    if(btnAnterior != 'C')
+                    {
+                        let res = eval(inpTxt.value);  // calcular resultado
+                        console.log(res);
+                        inpTxt.value = res;
+                    }
+                }
+            }            
+        }        
+    }
+    btnAnterior = txtBtn;
 }
