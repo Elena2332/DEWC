2e632b8ffcb50d7e020d20d6dd306643ba660b86
Elena2332 <elenafesional@gmail.com>
2022-10-13 14:29:40 +0200

    aaaa

--- 6_expresiones regulares/app.js
+++ 6_expresiones regulares/app.js
@@ -1,61 +1,61 @@
-let inpDni,inpNom,inpNac,inpNH,inpEmail,inpPag,inpPass;
 let btnComprobar;
+let arrInps;
 
 window.onload
 {
     btnComprobar = document.getElementById("btnComprobar"); 
     btnComprobar.addEventListener("click",comprobar);
-    let arrInps = document.getElementsByTagName('input');
+    arrInps = document.getElementsByTagName('input');
     for(let i=0; i<arrInps.length; i++)
-        arrInps[i].addEventListener("focusout",validar);   // "focusout" y "blur" evento cuanto pierde el foco
-        //validarCasilla(arrayInputs[i].id, arrayInputs[i].value);
-    
+        arrInps[i].addEventListener("focusout",validar);   // "focusout" y "blur" evento cuanto pierde el foco    
 }
-function validar()
+function validar(aux)
 {
-    let id = this.id;
-    let texto = this.value;
-    if(texto.length > 0)
+    // let id;
+    // if(!esInp(this.id))
+    //     id = this.id;
+    // else
+    //     id = aux;
+    // let inp = document.getElementById(id);    
+    if(inp.value != undefined)
     {
-        switch (id) 
+        let texto = inp.value;
+        switch(id) 
         {
             case 'inpDNI':
                 if (!(/^\d{8}[a-zA-Z]$/.test(texto)))
-                    document.getElementById(id).value = "";
+                    inp.value = "";
                 break;
             case 'inpNom':
                 if (!(/^[a-zA-Z ]*$/.test(texto)))
-                    document.getElementById(id).value = "";
+                    inp.value = "";
                 break;
             case 'inpNH':
                 if (texto<0 || texto>10)
-                    document.getElementById(id).value = "";
-                break;
-            case 'inpFecha':
-                if (!(/^$/.test(texto)))
-                    document.getElementById(id).value = "";
+                    inp.value = "";
                 break;
             case 'inpEmail':
                 if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(texto)))
-                    document.getElementById(id).value = "";
-                break;
-            case 'inpPag':
-                if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(texto)))
-                    document.getElementById(id).texto = "";
+                    inp.value = "";
                 break;
             case 'inpPass':
                 if (!(/(^[0-9\s\+\-])+$/.test(texto)))
-                    document.getElementById(id).value = "";
+                    inp.value = "";
                 break;
         }
-    }
-    
+    }    
 }
-
 
 function comprobar()
 {
-    console.log('aaaaaaa');
-    validar();
+    for(let i=0; i<arrInps.length; i++)
+        validar(arrInps[i]);
 }
 
+function esInp(str)
+{
+    for(let i=0; i<arrInps.length; i++)
+        if(arrInps[i]==str)
+            return true;
+    return false;
+}
