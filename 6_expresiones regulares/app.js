b73bbc4806f6a7bd12cc8b6a93512431e8fb74ba
Elena2332 <elenafesional@gmail.com>
2022-10-18 13:25:38 +0200

    muerte dolor y sufrimiento

--- 6_expresiones regulares/app.js
+++ 6_expresiones regulares/app.js
@@ -4,7 +4,7 @@ let arrInps;
 window.onload
 {
     btnComprobar = document.getElementById("btnComprobar"); 
-    btnComprobar.addEventListener("click",comprobar);
+    btnComprobar.addEventListener("click",validar);
     arrInps = document.getElementsByTagName('input');
     for(let i=0; i<arrInps.length; i++)
         arrInps[i].addEventListener("focusout",validar);   // "focusout" y "blur" evento cuanto pierde el foco    
@@ -42,7 +42,15 @@ function validar(aux)
                 break;
             case 'inpNH':
                 if (texto<0 || texto>10)
-                if (!(/^[a-zA-Z ]*$/.test(texto)))
+                {
+                    inp.value = "";
+                    inp.style = "border: 1px solid red; border-radius:5px;"
+                }
+                else
+                    inp.style = "";
+                break;
+            case 'inpFecha':
+                if (!(/^(?:(?:(?:0?[1-9]|1\d|2[0-8])[/](?:0?[1-9]|1[0-2])|(?:29|30)[/](?:0?[13-9]|1[0-2])|31[/](?:0?[13578]|1[02]))[/](?:0{2,3}[1-9]|0{1,2}[1-9]\d|0?[1-9]\d{2}|[1-9]\d{3})|29[/]0?2[/](?:\d{1,2}(?:0[48]|[2468][048]|[13579][26])|(?:0?[48]|[13579][26]|[2468][048])00))$/.test(texto)))
                 {
                     inp.value = "";
                     inp.style = "border: 1px solid red; border-radius:5px;"
@@ -52,6 +60,15 @@ function validar(aux)
                 break;
             case 'inpEmail':
                 if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(texto)))
+                {
+                    inp.value = "";
+                    inp.style = "border: 1px solid red; border-radius:5px;"
+                }
+                else
+                    inp.style = "";
+                break;
+            case 'inpPag':
+                if (!(/^\w+([\.-]?\w+)*.\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(texto)))
                 {
                     inp.value = "";
                     inp.style = "border: 1px solid red; border-radius:5px;"
---
+++ 7_segurocochePlantilla/js/appes6.js
@@ -0,0 +1,68 @@
+let selMarca, selAnio, inpTipo, btnCotizar, divRes;
+const BASICO = 2000;
+window.onload
+{
+    selMarca = document.getElementById("marca");
+    selAnio = document.getElementById("anio");
+    inpTipo = document.getElementsByName("tipo");
+    divRes = document.getElementById("resultado");
+    llenarAnios();
+    btnCotizar = document.getElementsByTagName("button")[0];
+    btnCotizar.addEventListener("click",calcular);
+}
+
+
+function calcular()
+{
+    let marca = selMarca.value;
+    let anio = selAnio.value;
+    let tipo = inpTipo.value;
+    let anioActual = new Date().getFullYear();
+    let res = 0; 
+    let bien = true;
+    //Procedencia
+    switch(marca)
+    {
+        case 1:
+            res = BASICO + (BASICO*0.15);
+            console.log('a');
+            break;
+        case 2:
+            res = BASICO + (BASICO*0.05);
+            console.log('b');
+            break;
+        case 3:
+            res = BASICO + (BASICO*0.35);
+            console.log('c');
+            break;
+        default:
+            console.log('d');
+            bien = false;
+            break;
+    }
+    if(!bien)
+        divRes.innerHTML = '<p class="error">INTRODUCE MARCA</p>';
+    else
+    {
+        //Tipo
+        if(tipo == "basico")
+            res = res + (res*0.3);
+        else
+            res = res + (res*0.5);
+        console.log(res);
+        //Anio
+        if(anioActual-anio >= 10)    // descuento maximo = 30%
+            res = res - (res*(0.3));
+        else
+            res = res - (res*(anio*3/100)); 
+        console.log(res);
+        divRes.innerHTML= '<p class="correcto">Total: '+res+'</p>';
+    }
+}
+
+function llenarAnios()
+{
+    let anioActual = new Date().getFullYear();
+    for(let i=0,anio=anioActual; anio>=1930 ;i++,anio--)
+        selAnio.options[i] = new Option(anio, i);
+}
