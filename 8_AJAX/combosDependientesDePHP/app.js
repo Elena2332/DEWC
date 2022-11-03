9199d3c648b362448713a2de6b736aa45c45ea51
Elena2332 <elenafesional@gmail.com>
2022-11-03 12:34:19 +0100

    /
    no me gusta es ejercicio

--- 8_AJAX/combosDependientesDePHP/app.js
+++ 8_AJAX/combosDependientesDePHP/app.js
@@ -2,21 +2,58 @@ let prov,muni;
 
 window.onload
 {
-    prov = document.getElementById("provincia");
-    muni = document.getElementById("municipio");
-    prov.addEventListener("change",llenarMunicipio);
-    muni.addEventListener("change",mostrarMensaje);
+    prov = document.getElementById("provincia").onchange = cargarMunicipios;
+    muni = document.getElementById("municipio").onchange = mostrarMensaje;
+    // prov.addEventListener("change",cargarMunicipios);
+    // muni.addEventListener("change",mostrarMensaje);
 
     llenarProvincia();
 }
 
 
+
+function inicializa_xhr()
+{
+    if(window.XMLHttpRequest)
+        return new XMLHttpRequest;
+    else if(window.ActiveXObject)
+        return ActiveXObject("Microsoft.XMLHTTP");
+}
+
 function llenarProvincia()
+{
+    console.log("Cargando provincias");
+
+    peticion = inicializa_xhr();
+    if(peticion)
+    {
+        peticion.onreadystatechange = muestraProvincias;
+        peticion.open("GET","php/cargaProvinciasJSON.php",true);
+        peticion.send(null);
+    }
+    muestraProvincias;
+}
+
+function muestraProvincias()
+{
+    if(peticion.readyState == 4)
+        if(peticion.status == 200)
+            console.log(peticion.responseText);
+}
+
+function mostrarProvincias()
+{
+    if(peticion.readyState == 4)
+        if(peticion.status == 200)
+        {
+            console.log(peticion.responseText);
+        }    
+}
+
+function cargarMunicipios()
 {
     
 }
-
-
 
 function mostrarMensaje()
 {
