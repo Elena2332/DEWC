5cb267d8c72ccc04c06e0bcf9840a106e367cc69
Elena2332 <elenafesional@gmail.com>
2022-11-07 10:59:34 +0100

    hay fallos
    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- 8_AJAX/combosDependientesDePHP/app.js
+++ 8_AJAX/combosDependientesDePHP/app.js
@@ -2,12 +2,12 @@ let prov,muni;
 
 window.onload
 {
-    prov = document.getElementById("provincia").onchange = cargarMunicipios;
-    muni = document.getElementById("municipio").onchange = mostrarMensaje;
-    // prov.addEventListener("change",cargarMunicipios);
-    // muni.addEventListener("change",mostrarMensaje);
+    prov = document.getElementById("provincia")//.onchange = cargarMunicipios;
+    muni = document.getElementById("municipio")//.onchange = mostrarMensaje;
+    prov.addEventListener("change",cargarMunicipios);
+    muni.addEventListener("change",mostrarMensaje);
 
-    llenarProvincia();
+    cargarProvincia();
 }
 
 
@@ -20,7 +20,7 @@ function inicializa_xhr()
         return ActiveXObject("Microsoft.XMLHTTP");
 }
 
-function llenarProvincia()
+function cargarProvincia()
 {
     console.log("Cargando provincias");
 
@@ -28,7 +28,7 @@ function llenarProvincia()
     if(peticion)
     {
         peticion.onreadystatechange = muestraProvincias;
-        peticion.open("GET","php/cargaProvinciasJSON.php",true);
+        peticion.open("GET","cargaProvinciasJSON.php",true);
         peticion.send(null);
     }
     muestraProvincias;
@@ -47,13 +47,63 @@ function mostrarProvincias()
         if(peticion.status == 200)
         {
             console.log(peticion.responseText);
+            let provincias = eval('('+peticion.responseText+')');
+            provinciasArray = provincias;
+
+            console.log(provinciasArray);
+
+            prov.options[0] = new Option("- selecciona -");
+            let i = 1;
+            for(let codigo in provincias)
+            {
+                prov.options[i] = new Option(provincias[codigo],codigo);
+                i++;    
+            }
         }    
 }
 
 function cargarMunicipios()
 {
-    
+    console.log("cargando municipios");   
+    let provincia = prov.options[prov.selectedIndex].value;
+
+    console.log(provincia);
+
+    if(!isNaN(provincia))
+    {
+        peticion = inicializa_xhr();
+        if(peticion)
+        {
+            peticion.onreadystatechange = muestraMunicipios;
+            peticion.open("POST","cargarMunicipiosJSON.php?nocache="+Math.random(),true);
+            peticion.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+            peticion.send("provincia="+provincia);
+        }
+    }
 }
+
+function muestraMunicipios()
+{
+    if(peticion.readyState == 4)
+    {
+        if(peticion.status == 200)
+        {
+            let municipios = eval('('+peticion.responseText+')');
+            municipiosArray = municipios;
+            console.log(municipiosArray);
+
+            muni.options.length = 0;
+            let i = 0;
+            for(let codigo in municipios)
+            {
+                muni.options[i] = new Option(municipios[codigo],codigo);
+                i++;
+            }
+        }
+    }    
+}
+
+
 
 function mostrarMensaje()
 {
